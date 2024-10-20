4413fa8040e3fcdf86aa5418798702885d55c67e
ponchaud savannah <ponchaud@et.esiea.fr>
2024-10-20 18:21:21 +0200

    started working on bitmap

--- class.c
+++ class.c
@@ -8,12 +8,69 @@ typedef struct student {
     int age;
     int ID;
     long int date;
+    long int last_modif;
     float notes[40];
+    unsigned int status; // bit 1 : inscrit, bit 2 : en atente, bit 3 : dipome, bit 4 : suspendu 
+    unsigned int languages; // bit 1 : Anglais, bit 2 : francais, bit 3 : allemend, bit 4 : espagnol, bit 5 : italien, bit 6 : latin, bit 7 : grec, bit 8 : russe
     struct student *next;
 } etudiant;
 
+void set_bit(unsigned int* bitmap, int n) {
+    *bitmap |= (1 << n);  // Active le n-ième bit
+}
+
+void clear_bit(unsigned int* bitmap, int n) {
+    *bitmap &= ~(1 << n);  // Désactive le n-ième bit
+}
+
+int is_bit_set(unsigned int bitmap, int n) {
+    return (bitmap & (1 << n)) != 0;  // Vérifie si le n-ième bit est activé
+}
+
 void affiche_student(etudiant* ed) {
-    printf("Student :\nNom : %s\nPrénom : %s\nÂge : %d\n", ed->nom, ed->prenom, ed->age);
+    struct tm* local_time = localtime(&ed->date);
+    char* date_str = (char*)malloc(100 * sizeof(char)); 
+     if (date_str == NULL) {
+        perror("Erreur d'allocation de mémoire");
+        return;
+    }
+    struct tm* local_time2 = localtime(&ed->last_modif);
+    char* date_str2 = (char*)malloc(100 * sizeof(char)); 
+     if (date_str == NULL) {
+        perror("Erreur d'allocation de mémoire");
+        return;
+    }
+    strftime(date_str, 100, "%d/%m/%Y %H:%M:%S", local_time);
+    strftime(date_str2, 100, "%d/%m/%Y %H:%M:%S", local_time2);
+    printf("Student :\nNom : %s\nPrénom : %s\nÂge : %d\nID : %d\nDate de création : %s\nDerniere modif : %s\n", ed->nom, ed->prenom, ed->age,ed->ID,date_str,date_str2);
+    free(date_str);
+    free(date_str2);
+}
+
+void modiify_student(etudiant* ptr){
+    affiche_student(ptr);
+    printf("que voulez vous modifier?\n1 : nom\n2 : prénom\n3 : age\n");
+    int h;
+    scanf(" %d", &h);
+    switch (h)
+    {
+    case 1:
+        printf("entrez le nouveau nom de l'étudiant");
+        scanf("%s", ptr->nom);
+        break;
+    case 2:
+        printf("entrez le nouveau prénom de l'étudiant");
+        scanf("%s", ptr->prenom);
+        break;
+    case 3 :
+        printf("entrez le nouvel age de l'étudiant");
+        scanf("%d", &ptr->age);
+        break;
+    default:
+        break;
+    }
+    time(&ptr->last_modif);
+    affiche_student(ptr);
 }
 
 void affiche_list(etudiant* head) {
@@ -41,6 +98,7 @@ etudiant* insert(etudiant* lstptr, int nb) {
     scanf("%d", &ptr->age);  // Attention : il faut passer l'adresse de ptr->age avec &
     ptr->ID = nb;
     time(&ptr->date);
+    time(&ptr->last_modif);
 
     ptr->next = lstptr;  // Insérer le nouvel étudiant au début de la liste
     return ptr;  // Retourner le nouvel élément en tête de liste
@@ -55,11 +113,19 @@ void free_list(etudiant* head) {
     }
 }
 
+etudiant* select_student(etudiant* head, int index){
+    //pas oublier faire une copie du pointeur avant de le passer a la focntion select
+    while (head != NULL || head->ID != index){
+        head = head->next;
+    }
+    return head;
+}
+
 void write_list(etudiant *head){
-    FILE * fp = fopen("databin", "ab");
+    FILE * fp = fopen("databin", "wb");
     etudiant* tmp = head;
     while (tmp != NULL) {
-        fwrite(&tmp, sizeof(etudiant), 1, fp);
+        fwrite(tmp, sizeof(etudiant), 1, fp);
         tmp = tmp->next;
     }
     fclose(fp);
@@ -106,22 +172,54 @@ etudiant* recup_list() {
 
 int main() {
     etudiant* ptr = NULL;  // Initialiser la liste à NULL
+    etudiant* ptr2;
     ptr  = recup_list();
     int debut;
     if (ptr == NULL){
         debut = 0;
     }
     else{
-        debut = ptr->ID;
+        debut = ptr->ID +1;
+    }
+    int y = 1;
+    char t;
+    int s;
+    while(y){
+        printf("Menu principal :\nq to quit\na to add a student\nd to display the students list\nm to modify a student\n");
+        scanf(" %c",&t);
+        switch (t)
+        {
+        case 'q':
+            y = 0;
+            break;
+        case 'a':
+            ptr = insert(ptr,debut);
+            debut++;
+            break;
+        case 'd':
+            affiche_list(ptr);
+            break;
+        case 'm':
+            affiche_list(ptr);
+            printf("select the student number you wish to modify\n");
+            scanf(" %d",&s);
+            ptr2 = ptr;
+            ptr2 = select_student(ptr2, s);
+            if(ptr2 == NULL){
+                printf("le numéro que vous avez rentré n'existe pas");
+            }
+            else{
+                modiify_student(ptr2);
+            }
+        
+        }
     }
     // Insérer 5 étudiants dans la liste
-    for (int i = 0; i < 3; i++) {
+    /*for (int i = 0; i < 3; i++) {
         debut++;
-        ptr = insert(ptr,debut);  // Insérer un nouvel étudiant dans la liste
-         // Afficher la liste après chaque insertion
-        
-    }
-    affiche_list(ptr); 
+        ptr = insert(ptr,debut);  
+    }*/
+    
     write_list(ptr);
 
     // Libérer la mémoire allouée pour la liste
